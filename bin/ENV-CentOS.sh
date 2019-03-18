31c8f598633404f0e2db7d15474ea1887b5256c9
Joe <joe@xxtime.com>
2019-03-18 14:13:46 +0800

    yum install php72

--- bin/ENV-CentOS.sh
+++ bin/ENV-CentOS.sh
@@ -174,6 +174,8 @@ function install_mysql(){
 }
 
 
+# yum install php72 php72-php-fpm php72-php-gd php72-php-pdo php72-php-phalcon3 php72-php-mysqlnd php72-php-pecl-redis php72-php-pecl-mongodb
+# 启动php-fpm命令  systemctl start php72-php-fpm.service
 function install_php(){
     exist=`ls /usr/local | grep php | wc -l`
     if [ $exist -gt 0 ]; then
